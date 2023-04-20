ee88fe402131af42c7d9a0ea0f6cd57ea5d10581
lat9723 <lauraagustinatrotta@gmail.com>
2023-04-19 23:11:12 -0300

    adding connection with front end stored in https://github.com/lat9723/movie-recommender

--- index.js
+++ index.js
@@ -1,36 +1,58 @@
 import { ChatGPTAPI } from 'chatgpt'
-import  express from 'express'
-import * as dotenv from 'dotenv'
+import express from 'express'
+import dotenv from 'dotenv'
 dotenv.config()
 
-const app = express();
+const app = express()
 
+
+// Enable CORS middleware
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
+  next()
+})
 
 // Route to generate movie recommendations
 app.get('/recommendations', async (req, res) => {
-  const { movie } = req.query;
+  const { director } = req.query
 
   // Generate the prompt to send to OpenAI
-  const prompt  = `Generate movie recommendations for "${movie}" but with a different director`;
-
+  const prompt  = `Generate movie recommendations for "${director}" but with a different director`;
+  console.log(prompt);
 
   try {
+    const api = new ChatGPTAPI({
+      apiKey: process.env.CHAT_GPT_KEY
+    })
 
-  const api = new ChatGPTAPI({
-    apiKey: process.env.CHAT_GPT_KEY
-  })
+    // async function testAPI() {
+    //   try {
+    //     const prompt = 'Hello, can you recommend a good movie?'
+    //     const response = await api.sendMessage(prompt)
+    //     console.log(response.text)
+    //   } catch (error) {
+    //     console.error(error);
+    //   }
+    // }
+    
+    // testAPI()
 
-  const response = await api.sendMessage(prompt)
+    const response = await api.sendMessage(prompt)
 
-    res.send(response.text);
+    res.send(response.text)
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Error generating movie recommendations');
+    console.error(error)
+    res.status(500).send('Error generating movie recommendations')
   }
-});
+})
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+  console.log(`Server listening on port ${port}`)
+})
